00dc98933c59afc68728ac448dca119215584bf5
soumaya rihane <soumayarihane@gmail.com>
2020-03-12 17:50:59 +0100

    [ADD]

--- school/school/wizard/Add_student.py
+++ school/school/wizard/Add_student.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from addons.hr_pyzk.models import device_users
 
 
 class Add_student(models.TransientModel):
@@ -6,20 +7,22 @@ class Add_student(models.TransientModel):
     _description = 'Add student'
 
     student_name = fields.Many2many('student.student','student_name',string='student_name')
+    device_id = fields.Many2one('devices', 'Fingerprint Device')
     #id = fields.Many2many('student.student')
 
     @api.multi
     def add_button(self, vals):
         # student =self.env['student.student'].search([('standard_id','=','')])
-        if self.student_name:
-
-            for student in self.student_name:
-                self.env['device.users'].create({'name': student.name, 'device_user_id': student.id})
+        if self.student_name and self.device_id:
+         for student in self.student_name:
+           for device in self.device_id:
+                self.env['device.users'].create({'name': student.name, 'device_user_id': student.id,'device_id': device.id})
                 # print(rec.student_name)
         else:
             print("")
             # TODO ADD message  error
 
+        #device_users.create_user(self,device)
 
    # def add_button(self,vals):
     #  student=self.env['student.student'].search([])
