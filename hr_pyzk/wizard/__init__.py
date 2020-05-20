4184c6f46c90bad7072aa0f3fd1d9aa30235bae1
ameni <amanigharbi9@gmail.com>
2020-05-20 12:10:38 +0100

    [12.0][MOD][Description]

--- hr_pyzk/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import user_wizard
-from . import delete_attendance_wizard
-
--- school/ecole/wizard/__init__.py
+++
@@ -1,9 +0,0 @@
-# See LICENSE file for full copyright and licensing details.
-
-
-from . import move_standards
-from . import wiz_send_email
-from . import mettre_fin_raison
-from . import add_eleve
-from . import report_statistique_descipline
-from . import wizard_parametrage_descipline
