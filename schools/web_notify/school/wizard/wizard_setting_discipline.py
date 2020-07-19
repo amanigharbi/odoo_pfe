72d285311003fd163fe433048457d0a7673395c1
soumaya rihane <soumayarihane@gmail.com>
2020-07-19 17:22:03 +0100

    [ADD] description

---
+++ schools/web_notify/school/wizard/wizard_setting_discipline.py
@@ -0,0 +1,44 @@
+from odoo import models, fields, api
+
+
+
+class parametrage_discipline(models.TransientModel):
+    _name = 'settings.discipline'
+    _description = "settings discipline"
+
+    status_discipline=fields.Selection([('Absent','Absent'),('Late','Late')],'Choice Discipline')
+    number_avertissement=fields.Integer('Number of discipline choiced to get avertissement :')
+    number_exclu=fields.Integer('Number of avertissement to get exclu :')
+
+    max_late=fields.Float("Maximum late for a student")
+
+
+    def save_settings(self):
+        data = {
+            'ids': self.ids,
+            'model': self._name,
+            'form': self.read()[0]
+        }
+
+        new_nb_avert = data['form']['number_avertissement']
+        new_nb_exclu = data['form']['number_exclu']
+        new_max_late = data['form']['max_late']
+        new_status_discipline = data['form']['status_discipline']
+        if self.env['settings.discipline'].search_count([]) > 0:
+            search = self.env['settings.discipline'].search([])
+
+            for a in search:
+                a.unlink()
+
+            self.env['settings.discipline'].create({'number_avertissement':new_nb_avert,
+                    'number_exclu': new_nb_exclu,
+                    'max_late':new_max_late,'status_discipline':new_status_discipline,
+                     })
+        else:
+            self.env['settings.discipline'].create({'number_avertissement': new_nb_avert,
+                                                    'number_exclu': new_nb_exclu,
+                                                    'max_late': new_max_late,
+                                                    'status_discipline': new_status_discipline,
+                                                    })
+
+
